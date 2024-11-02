a11e11db1a01de11368e43c495a43f27c6f94e19
louisleger-code <louisleger6@gmail.com>
2024-11-02 21:37:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+    menu.classList.toggle('show');
 }
